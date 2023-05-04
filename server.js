b0fc957babecd629fc02464c6e3b2fded325f99e
user <palmtree8880@gmail.com>
2023-05-04 13:27:59 +0900

    add

--- server.js
+++ server.js
@@ -13,5 +13,16 @@ app.use('/api/users/', usersRoute)
 app.use('/api/buses/', busesRoute) 
 app.use('/api/bookings/', bookingsRoute) 
 
+if (process.env.NODE_ENV === 'production'){
+  app.use('/', express.static('client/build'))
+  app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res)=> {
+    res.send('Please set to production')
+  }) 
+}
+
 app.listen(port, ()=> console.log(`Node server listening on port ${port}!`));
 // app.get('/', (req, res) => res.send('Hello World'))
